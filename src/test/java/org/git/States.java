81462ae7b7c70ff77418ff75cf243f2b50d6d680
Yamini <myamini2010@gmail.com>
2023-03-12 14:18:53 +0530

    login

---
+++ src/test/java/org/git/States.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class States {
+	private void tamilNadu() {
+		System.out.println("TamilNadu");
+	}
+
+	private void kerala() {
+		System.out.println("Kerala");
+	}
+
+	private void Andhara() {
+		System.out.println("Andhara");
+	}
+}
